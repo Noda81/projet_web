5a743eeff5f4693054ed8b03968fafad8447024d
Noda81 <noa81150@gmail.com>
2026-03-05 17:45:01 +0100

    Links changes
    Changes to the way to pages and images are defined. Goal is to work on github Pages

--- pages/projets/image_liens.js
+++ pages/projets/image_liens.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
 
     const liens = {
-        "/images/icons/lua.png": "https://www.lua.org",
-        "/images/icons/python.png": "https://www.python.org/",
-        "/images/icons/love2d.png": "https://love2d.org/",
-        "/images/icons/solar2d.png": "https://solar2d.com/",
+        "../../images/icons/lua.png": "https://www.lua.org",
+        "../../images/icons/python.png": "https://www.python.org/",
+        "../../images/icons/love2d.png": "https://love2d.org/",
+        "../../images/icons/solar2d.png": "https://solar2d.com/",
     };
 
     document.addEventListener("click", (e) => {
