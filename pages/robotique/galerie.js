945357f020fbbef8e4b530021f9446f0dcc09af9
Noda81 <noa81150@gmail.com>
2026-03-06 23:53:29 +0100

    Fixes and changes
    Fixed broken links
    Updated the "Robotique" page :
    - New sections
    - New images in "Galerie"
    
    Known issues :
    - Temporary links / usernames in team contacts

--- pages/robotique/galerie.js
+++ pages/robotique/galerie.js
@@ -1,20 +1,29 @@
-const max_i = 2
+const max_i = 5
 const min_i = 1
 var index = 1;
 
 const images = {
-    1: "images/robot_plat.png",
-    2: "images/robot_haut.png"
+    1: "images/galerie/robot_plat.png",
+    2: "images/galerie/robot_haut.png",
+    3: "images/galerie/chassis_final.jpg",
+    4: "images/galerie/instructions.png",
+    5: "images/galerie/virtuel.png"
 };
 
 const titres = {
     1: "Robot plat",
-    2: "Robot haut"
+    2: "Robot haut",
+    3: "Châssis final",
+    4: "Instructions",
+    5: "Modélisation"
 };
 
 const descriptions = {
     1: "Architecture avec un châssis plat. Cette approche donne la priorité à la stabilité et permet d'avoir un robot prévisible.",
-    2: "Architecture avec un châssis haut. Cette approche met la physique au centre de son design, réduit les frottements avec le sol mais peut être moins stable."
+    2: "Architecture avec un châssis haut. Cette approche met la physique au centre de son design, réduit les frottements avec le sol mais peut être moins stable.",
+    3: "L'architecture de châssis retenue pour le robot final, pensée pour offrir les avantages des deux autre châssis.",
+    4: "Extrait des instructions de construction du robot",
+    5: "Construction virtuelle du robot pour manipuler sont architecture à tout moment."
 };
 
 function next() {
